ed89522cf54bcd440330f80fece1d14aed5f49b8
scmfaria <sara.moura.faria@gmail.com>
2020-02-10 13:34:57 -0300

    added jwt in update user

--- src/routes.js
+++ src/routes.js
@@ -5,10 +5,16 @@ import User from './app/models/User';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
+
+// aqui estou dizendo que todas as rotas que vem depois desse middleware vai usar ele!
+routes.use(authMiddleware);
+
 routes.put('/users', UserController.update);
 
 export default routes;
