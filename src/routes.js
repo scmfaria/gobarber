a6e825408c2b41787f2a65024c11a36aebf1a79e
scmfaria <sara.moura.faria@gmail.com>
2020-05-19 14:41:12 -0300

    get and update notifications

--- src/routes.js
+++ src/routes.js
@@ -10,7 +10,7 @@ import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
 import ScheduleController from './app/controllers/ScheduleController';
-
+import NotificationController from './app/controllers/NotificationController';
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
@@ -34,5 +34,9 @@ routes.post('/appointments', AppointmentController.store);
 routes.get('/appointments', AppointmentController.index);
 
 routes.get('/schedule', ScheduleController.index);
+
+routes.get('/notifications', NotificationController.index);
+
+routes.put('/notifications/:id', NotificationController.update);
 
 export default routes;
