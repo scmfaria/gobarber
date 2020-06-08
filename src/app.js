3a9090957eabc12178809396c55bbb1044a59b07
scmfaria <sara.moura.faria@gmail.com>
2020-06-08 09:33:07 -0300

    applying security in the application

--- src/app.js
+++ src/app.js
@@ -4,7 +4,12 @@ import express from 'express'; // add dependencia SUCRASE para permitir o javasc
 import 'express-async-errors';
 import routes from './routes';
 import path from 'path';
+import helmet from 'helmet';
+import redis from 'redis';
+import RateLimit from 'express-rate-limit';
+import RateLimitRedis from 'rate-limit-redis';
 import Youch from 'youch';
+
 
 // sentry - usado para tratamento de erros
 import * as Sentry from '@sentry/node'; 
@@ -26,11 +31,27 @@ class App {
   middlewares() {
     this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json());
+    this.server.use(helmet());
     this.server.use(
       '/files', 
       express.static(path.resolve(__dirname, '..', 'tmp', 'uploads'))
     );
     // express.static - lib que serve para enviar arquivos estaticos, como exemplo acesso em um link de imagem, etc
+  
+    if(process.env.NODE_ENV != 'development') {
+      this.server.use(
+        new RateLimit({
+          store: new RateLimitRedis({
+            client: redis.createClient({
+              host: process.env.REDIS_HOST,
+              port: process.env.REDIS_PORT,
+            }),
+          }),
+          windowMs: 1000 * 60 * 15,
+          max: 100,
+        })
+      );
+    }
   }
 
   routes() {
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,7 @@
 import { Router } from 'express'; // serve para separar a parte de roteamento em outro arquivo
 import multer from 'multer';
+import Brute from 'express-brute';
+import BruteRedis from 'express-brute-redis';
 import multerConfig from './config/multer';
 
 import User from './app/models/User';
@@ -18,9 +20,16 @@ import authMiddleware from './app/middlewares/auth';
 const routes = new Router();
 const upload = multer(multerConfig);
 
+const bruteStore = new BruteRedis({
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+});
+
+const bruteForce = new Brute(bruteStore);
+
 routes.post('/users', UserController.store);
 
-routes.post('/sessions', SessionController.store);
+routes.post('/sessions', bruteForce.prevent, SessionController.store);
 
 // aqui estou dizendo que todas as rotas que vem depois desse middleware vai usar ele!
 routes.use(authMiddleware);
